2f10b0c73cb6caacc605fea3430607d2c3ba922e
I1HN <yagoksn2010@gmail.com>
2026-03-25 08:28:11 -0300

    arrumado o codigo da multiplicação

---
+++ Script Operações.js
@@ -0,0 +1,23 @@
+let numUm, numDois, result;
+//// ADIÇÃO
+numUm = Number(prompt("me diga o primeiro valor da adição"));
+numDois = Number(prompt("me diga o segundo valor da adição"));
+
+alert(`o resultado da adição é: ${(numUm+numDois).toFixed(2)}`);
+//// SUBTRAÇÃO
+numUm = Number(prompt("Me mostre o primeiro número da subtração"));
+numDois = Number(prompt("Me mostre o segundo número da subtração"));
+
+alert(`o resultado da subtração é: ${(numUm-numDois).toFixed(2)}`);
+//// MULTIPLICAÇÃO
+numUm = Number(prompt("insira o primeiro número da multiplicação:"));
+numDois = Number(prompt("insira o segundo número da multiplicação:"));
+
+alert(`o resutado da sua multiplicação é: ${(numUm * numDois).toFixed(2)}`);
+//// DIVISÃO
+numUm = Number(prompt("informe o número que vai ser dividido"));
+numDois = Number(prompt("informe o número que vai dividir"));
+
+alert(
+  `o número ${numUm} dividido por ${numDois} resulta em ${(numUm / numDois).toFixed(2)}`,
+);
